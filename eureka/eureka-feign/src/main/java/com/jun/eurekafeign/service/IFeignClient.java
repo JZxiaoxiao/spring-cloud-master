da299d3e87839c5a66cd64d72bff28749af48f80
JZxiaoxiao <junxiaomao123@163.com>
2019-11-01 18:08:41 +0800

    post test.

--- eureka/eureka-feign/src/main/java/com/jun/eurekafeign/service/IFeignClient.java
+++ eureka/eureka-feign/src/main/java/com/jun/eurekafeign/service/IFeignClient.java
@@ -3,20 +3,21 @@ package com.jun.eurekafeign.service;
 import com.jun.eurekafeign.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @FeignClient(value = "service-provider")
 public interface IFeignClient {
-    //
-    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+
+    @GetMapping(value = "/hi")
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
 
-    @RequestMapping(value = "/say",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/say")
     String sayJsonFromClient(@RequestBody User data);
+
+    //效果等同
+//    @RequestMapping(value = "/say",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+//    String sayJsonFromClient(@RequestBody User data);
 
     @RequestMapping(value = "/me",method = RequestMethod.GET)
     String sayJsonStringFromClient(@RequestParam(value = "data") String data);
