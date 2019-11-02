dfe3d8cd04b7f1b91de238c11673befb34522ca4
JZxiaoxiao <junxiaomao123@163.com>
2019-11-02 13:07:11 +0800

    add eureka ha.

---
+++ eureka/eureka-server-ha/src/test/java/com/jun/eurekserverha/EurekaServerHaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jun.eurekserverha;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerHaApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
