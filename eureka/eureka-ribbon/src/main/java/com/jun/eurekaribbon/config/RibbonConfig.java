0e06933e6763b45a51fdd234e14d8fe35e918179
JZxiaoxiao <junxiaomao123@163.com>
2019-11-05 17:34:45 +0800

    add ribbon（edit config）.

---
+++ eureka/eureka-ribbon/src/main/java/com/jun/eurekaribbon/config/RibbonConfig.java
@@ -0,0 +1,23 @@
+package com.jun.eurekaribbon.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 第一种方式（不推荐）
+ * 全局配置
+ */
+@RibbonClient(configuration = RibbonConfig.DefaultRibbonConfig.class)
+public class RibbonConfig {
+
+    @Configuration
+    class DefaultRibbonConfig {
+        @Bean
+        public IRule ribbonRule() {
+            return new RandomRule();
+        }
+    }
+}
